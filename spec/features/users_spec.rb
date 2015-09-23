7dc4eb6956b50a376d913991917522fa90659f57
Andela Developer <andeladev@users-MacBook-Pro-3.local>
2015-09-23 09:43:39 +0100

    feature rspec test for users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -21,4 +21,24 @@ RSpec.feature "Users", type: :feature do
     click_link("Details")
     expect(current_path).to eq(statistics_path(link.id))
   end
+
+  scenario "only currently logged in users can view their own profile" do
+    user1 = User.create(name: "jeff", email: "seyi@andela.com", password: "seyijeff",
+      password_confirmation: "seyijeff")
+    user2 = User.create(name: "daisi", email: "daisi@andela.com", password: "sowemimo",
+      password_confirmation: "sowemimo")
+
+    visit root_path
+    click_link('Log in')
+    expect(current_path).to eq(login_path)
+    expect(page).to have_selector("div.logo", text: "Login")
+    fill_in "session[email]", with: 'seyi@andela.com'
+    fill_in "session[password]", with: 'seyijeff'
+    click_button('Log in')
+    expect(current_path).to eq(user_path(user1.id))
+
+    visit user_path(user2.id)
+
+    expect(current_path).to eq(root_path)
+  end
 end
